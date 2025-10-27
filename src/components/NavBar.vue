32c92d2fee5bd69f04ecdb6f16265bd59620ed38
abaransenol <business.alibaransenol@gmail.com>
2025-10-27 11:45:42 +0300

    NavBar.vue is made responsive!

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="fixed z-50 scroll-smooth w-screen p-3 border-b-2 bg-black opacity-50">
         <nav class="
-            max-w-fit m-auto text-slate-200 text-2xl space-x-5
+            max-w-fit m-auto text-slate-200 text-xl md:text-2xl space-x-3 md:space-x-5
             transform -translate-y-full opacity-0 duration-1000 ease-in-out"
             :class="{ 'translate-y-0 opacity-100': isNavBarVisible }"
         >
-            <a class="hover:text-3xl hover:opacity-50 duration-300" href="#">
+            <a class="inline-block hover:scale-115 hover:opacity-50 duration-300" href="#">
                 Home 
             </a>
-            <a class="hover:text-3xl hover:opacity-50 duration-300" href="#aboutme"> 
+            <a class="inline-block hover:scale-115 hover:opacity-50 duration-300" href="#aboutme"> 
                 About Me
             </a>
-            <a class="hover:text-3xl hover:opacity-50 duration-300" href="#projects">
+            <a class="inline-block hover:scale-115 hover:opacity-50 duration-300" href="#projects">
                 Projects
             </a>
-            <a class="hover:text-3xl hover:opacity-50 duration-300" href="#experience">
+            <a class="inline-block hover:scale-115 hover:opacity-50 duration-300" href="#experience">
                 Experience 
             </a>
         </nav>
