aa12f96ac578acdecfae87ddd0a26cb1d4be0205
abaransenol <business.alibaransenol@gmail.com>
2025-10-27 14:37:42 +0300

    AboutSection.vue is made responsive!

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -7,39 +7,39 @@
                 transform opacity-0 duration-1000 ease-in"
                 :class="{'-translate-1/2 opacity-100': isSectionVisible}"
             >
-                <h1 class="text-9xl w-fit m-auto">Hey!</h1>
-                <p class="text-3xl m-10 text-center">
+                <h1 class="text-8xl sm:text-9xl w-fit m-auto">Hey!</h1>
+                <p class="text-2xl m-6 sm:text-3xl sm:m-10 text-center">
                     I am a 20-year-old student who studies computer engineering in Eskisehir
                     Technical University. I have been interested in programming since I was 15 years
                     old. I have high level knowledge about dart language and flutter framework and
                     good enough knowledge about java and python languages.
                 </p>
                 <div class="flex flex-row w-fit m-auto opacity-80 space-x-3">
-                    <img class="w-20 h-20 hover:w-22 hover:h-22 duration-300 hover:cursor-pointer"
+                    <img class="w-15 h-15 hover:w-17 hover:h-17 sm:w-20 sm:h-20 sm:hover:w-22 sm:hover:h-22 duration-300 hover:cursor-pointer"
                         src="/src/assets/ico/langs/js.svg"
                         alt="js"
                         title="Javascript"
                         @click="() => redirect('https://developer.mozilla.org/en-US/docs/Web/JavaScript')"
                     >
-                    <img class="w-20 h-20 hover:w-22 hover:h-22 duration-300 hover:cursor-pointer"
+                    <img class="w-15 h-15 hover:w-17 hover:h-17 sm:w-20 sm:h-20 sm:hover:w-22 sm:hover:h-22 duration-300 hover:cursor-pointer"
                         src="/src/assets/ico/langs/vue.svg"
                         alt="vue"
                         title="Vue.js"
                         @click="() => redirect('https://vuejs.org/')"
                     >
-                    <img class="w-20 h-20 hover:w-22 hover:h-22 duration-300 hover:cursor-pointer"
+                    <img class="w-15 h-15 hover:w-17 hover:h-17 sm:w-20 sm:h-20 sm:hover:w-22 sm:hover:h-22 duration-300 hover:cursor-pointer"
                         src="/src/assets/ico/langs/flutter.svg"
                         alt="flutter"
                         title="Flutter"
                         @click="() => redirect('https://flutter.dev/')"
                     >
-                    <img class="w-20 h-20 hover:w-22 hover:h-22 duration-300 hover:cursor-pointer"
+                    <img class="w-15 h-15 hover:w-17 hover:h-17 sm:w-20 sm:h-20 sm:hover:w-22 sm:hover:h-22 duration-300 hover:cursor-pointer"
                         src="/src/assets/ico/langs/python.svg"
                         alt="python"
                         title="Python"
                         @click="() => redirect('https://www.python.org/')"
                     >
-                    <img class="w-20 h-20 hover:w-22 hover:h-22 duration-300 hover:cursor-pointer"
+                    <img class="w-15 h-15 hover:w-17 hover:h-17 sm:w-20 sm:h-20 sm:hover:w-22 sm:hover:h-22 duration-300 hover:cursor-pointer"
                         src="/src/assets/ico/langs/java.svg"
                         alt="java"
                         title="Java"
